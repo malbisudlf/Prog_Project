4713aac7015cb3aa85fe09cb7c6dc0a0212ce9ad
mikel.albisudela <mikel.albisudela@10.164.49.161>
2024-10-21 10:29:33 +0200

    con main

---
+++ src/pruba_package/Main.java
@@ -0,0 +1,12 @@
+package pruba_package;
+
+import javax.swing.SwingUtilities;
+
+public class Main {
+    public static void main(String[] args) {
+        
+            new AhorcadoGUI(); 
+    }
+}
+
+
